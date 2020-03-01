bec11b13af3412350ad3616dddabf797c204ae71
Priyanka <p.priyapatil579@gmail.com>
2020-03-01 18:48:50 +0530

    added evenno program

---
+++ Ex_evenno.java
@@ -0,0 +1,17 @@
+
+
+public class Ex_evenno {
+
+	public static void main(String[] args) {
+	 int num=2;
+	do
+	{
+		System.out.println(num);
+		num=num+2;
+	}
+
+		while(num<=100);
+
+	}
+
+}
---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test1
+{
+
+ 	public static void main(String[] args) {
+		System.out.println("Enter the string");
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        String[] words = s.trim().split(" ");
+        System.out.println("Number of words in the string = "+words.length);
+           sc.close();
+	}
+}
